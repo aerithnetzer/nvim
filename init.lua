dd1db78337c387a2a13593eae9abd4fe19984763
aerithnetzer <aerithnetzer@outlook.com>
2024-02-01 16:28:55 -0600

    Add copilot chat

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.g.copilot_assume_mapped = true
+vim.g.livepreview_previewer = "sioyek"
+vim.g.notes_directories = { "~/warlock/card-file" }
+vim.g.livepreview_engine = "xelatex"
