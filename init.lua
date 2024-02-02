1cbfda22fec570ba8bdd7ca9a069302a38a5a674
aerithnetzer <aerithnetzer@outlook.com>
2024-02-01 18:41:31 -0600

    Add markdown preview

--- init.lua
+++ init.lua
@@ -4,3 +4,6 @@ vim.g.copilot_assume_mapped = true
 vim.g.livepreview_previewer = "sioyek"
 vim.g.notes_directories = { "~/warlock/card-file" }
 vim.g.livepreview_engine = "xelatex"
+
+-- set spellcheck for markdown in lazyvim
+vim.cmd("autocmd FileType markdown setlocal spell")
