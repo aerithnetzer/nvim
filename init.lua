7916612eac5580effc5915997065b21a529dd34d
aerithnetzer <aerithnetzer@outlook.com>
2024-02-04 00:02:20 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@ vim.g.copilot_assume_mapped = true
 vim.g.livepreview_previewer = "sioyek"
 vim.g.notes_directories = { "~/warlock/card-file" }
 vim.g.livepreview_engine = "xelatex"
+vim.g.vimtex_compiler_method = "latexmk"
 
 -- set spellcheck for markdown in lazyvim
 vim.cmd("autocmd FileType markdown setlocal spell")
