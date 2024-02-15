1786e90f703a492f6d9c407ccf5d41fb72ebba9e
Kat Nykiel <katanykiel@gmail.com>
2024-02-14 21:11:57 -0500

    nvim config for her bc her hands are sore <3

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 require("config.lazy")
 
 vim.g.copilot_assume_mapped = true
-vim.g["pandoc#filetypes#handled"] = { "pandoc", "markdown" }
+vim.g["pandoc#filetypes#handled"] = {"pandoc", "markdown", "latex"}
 vim.g["pandoc#filetypes#pandoc_markdown"] = 0
 vim.g["pandoc#biblio#sources"] = "b"
 vim.g["pandoc#biblio#use_bibtool"] = 1
 vim.g["pandoc#completion#bib#mode"] = "citeproc"
-vim.g["zotcite_wait_attachment"] = 1
+vim.g["zotcite_wait_attachment"] = 0
 vim.g.copilot_assume_mapped = true
 vim.g.livepreview_previewer = "sioyek"
 vim.g.notes_directories = { "~/warlock/card-file" }
