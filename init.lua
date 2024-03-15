456b260a621e1766ed821a07f49754f0b5e4d752
aerithnetzer <aerithnetzer@outlook.com>
2024-03-15 10:53:27 -0500

    add plugins

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("mason").setup()
 
 vim.g.copilot_assume_mapped = true
-vim.g["pandoc#filetypes#handled"] = {"pandoc", "markdown", "latex"}
+vim.g["pandoc#filetypes#handled"] = { "pandoc", "markdown", "latex" }
 vim.g["pandoc#filetypes#pandoc_markdown"] = 0
 vim.g["pandoc#biblio#sources"] = "b"
 vim.g["pandoc#biblio#use_bibtool"] = 1
@@ -14,7 +15,6 @@ vim.g.notes_directories = { "~/warlock/card-file" }
 vim.g.livepreview_engine = "latexmk"
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g["pandoc#biblio#bibtool_extra_args"] = "-r biblatex"
-
 return {
   "folke/which-key.nvim",
   opts = function(_, opts)
