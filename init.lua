a67761a4369e02742c7886b49a3d7cfc390c0593
aerithnetzer <aerithnetzer@outlook.com>
2024-07-01 20:00:10 -0500

    Update because my damn laptop no workie

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("mini.move").setup()
-require("papis").setup({ enable_keymaps = true })
-
--- Remap <Caps_Lock> to <Esc> (if the terminal emulator sends a custom sequence for Caps Lock)
-vim.api.nvim_set_keymap("i", "<CapsLockSequence>", "<Esc>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<CapsLockSequence>", "<Esc>", { noremap = true })
-
+require("papis").setup({
+  -- Enable the default keymaps (defaults to `false`)
+  enable_keymaps = true,
+  -- You might want to change the filetypes activating papis.nvim
+  -- init_filetypes = { "markdown", "norg", "yaml" },
+  -- If you don't have an appropriate font (like Nerd Font), you
+  -- may want to disable icons. This may require a `:Papis reload data`.
+  -- to take effect.
+  -- enable_icons = false,
+})
 -- Set colorscheme
+vim.cmd.colorscheme("catppuccin")
--- lua/plugins/papis.lua
+++ lua/plugins/papis.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "localpapis",
-    dir = { "~/warlock/papis.nvim" },
-    dependencies = {
-      "kkharji/sqlite.lua",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("papis").setup({
-        -- Your configuration goes here
-        papis_python = {
-          dir = "~/Documents/papers",
-          info_name = "info.yaml",
-          notes_name = [[notes.norg]],
-        },
-        enable_keymaps = true,
-        init_filetypes = { "markdown", "org", "yaml" },
-      })
-    end,
+  "jghauser/papis.nvim",
+  dependencies = {
+    "kkharji/sqlite.lua",
+    "MunifTanjim/nui.nvim",
+    "pysan3/pathlib.nvim",
+    "nvim-neotest/nvim-nio",
+    -- if not already installed, you may also want:
+    -- "nvim-telescope/telescope.nvim",
+    -- "hrsh7th/nvim-cmp",
   },
+  config = function()
+    require("papis").setup({
+      -- Your configuration goes here
+      papis_python = {
+        dir = "/Users/ysc4337/OneDrive - Northwestern University/library",
+        info_name = "info.yaml",
+        notes_name = [[notes.norg]],
+      },
+      enable_keymaps = true,
+      init_filetypes = { "markdown", "norg", "yaml" },
+    })
+  end,
 }
