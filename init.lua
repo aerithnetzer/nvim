036d667abe5645d21c64043d253e6cd3bbd057f4
aerithnetzer <aerithnetzer@outlook.com>
2024-08-26 16:19:56 -0500

    Change some colorscheme

--- init.lua
+++ init.lua
@@ -1,15 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("mini.move").setup()
-require("papis").setup({
-  -- Enable the default keymaps (defaults to `false`)
-  enable_keymaps = true,
-  -- You might want to change the filetypes activating papis.nvim
-  -- init_filetypes = { "markdown", "norg", "yaml" },
-  -- If you don't have an appropriate font (like Nerd Font), you
-  -- may want to disable icons. This may require a `:Papis reload data`.
-  -- to take effect.
-  -- enable_icons = false,
-})
--- Set colorscheme
-vim.cmd.colorscheme("catppuccin")
+-- disable copilot on start
+vim.g.copilot_disable = true
+vim.o.wrap = true
+vim.o.wrap = true
