2956f88e8ad307cd1de779a40617d711820bd160
aerithnetzer <aerithnetzer@outlook.com>
2024-09-13 14:31:19 -0500

    Fix init.lua colorschemes

--- init.lua
+++ init.lua
@@ -5,9 +5,3 @@ require("mini.move").setup()
 vim.g.copilot_disable = true
 vim.o.wrap = true
 vim.o.wrap = true
-
--- set the colo scheme to default
-vim.cmd("colorscheme default")
-
--- make the neovim background transparent
-vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
