f2c76d4719ed2e5fe9fccf867d932f40d43eade0
aerithnetzer <aerithnetzer@outlook.com>
2025-02-03 19:42:51 -0600

    Refactor entire setup

--- init.lua
+++ init.lua
@@ -1,19 +1,50 @@
--- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
--- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
-local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
-end
-vim.opt.rtp:prepend(lazypath)
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
--- validate that lazy is available
-if not pcall(require, "lazy") then
-  -- stylua: ignore
-  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
-  vim.fn.getchar()
-  vim.cmd.quit()
-end
+-- boostrap mini plugins
+require("mini.comment").setup({})
+require("mini.icons").setup({})
+require("mini.surround").setup()
+require("mini.move").setup()
 
-require "lazy_setup"
-require "polish"
+-- boostrap LSP and LSP server config
+require("lspconfig").ruff.setup({})
+require("lspconfig").pyright.setup({})
+require("lspconfig").tinymist.setup({})
+require("lualine").setup({
+  options = {
+    theme = {
+      normal = {
+        a = { fg = "#ffffff", bg = "#0000ff", gui = "bold" },
+        b = { fg = "#ffffff", bg = "#000080" },
+        c = { fg = "#ffffff", bg = "#000040" },
+      },
+      insert = {
+        a = { fg = "#000000", bg = "#00ff00", gui = "bold" },
+        b = { fg = "#000000", bg = "#008000" },
+        c = { fg = "#000000", bg = "#004000" },
+      },
+      visual = {
+        a = { fg = "#000000", bg = "#ff0000", gui = "bold" },
+        b = { fg = "#000000", bg = "#800000" },
+        c = { fg = "#000000", bg = "#400000" },
+      },
+      replace = {
+        a = { fg = "#000000", bg = "#ffff00", gui = "bold" },
+        b = { fg = "#000000", bg = "#808000" },
+        c = { fg = "#000000", bg = "#404000" },
+      },
+      command = {
+        a = { fg = "#000000", bg = "#ff00ff", gui = "bold" },
+        b = { fg = "#000000", bg = "#800080" },
+        c = { fg = "#000000", bg = "#400040" },
+      },
+      inactive = {
+        a = { fg = "#808080", bg = "#303030", gui = "bold" },
+        b = { fg = "#808080", bg = "#303030" },
+        c = { fg = "#808080", bg = "#303030" },
+      },
+    },
+  },
+})
+vim.api.nvim_set_keymap("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,78 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
+
+      -- see below for full list of optional dependencies 👇
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/warlock/quartz/content",
+        },
+      },
+    },
+    -- see below for full list of options 👇
+  },
+  {
+    "amitds1997/remote-nvim.nvim",
+    version = "*", -- Pin to GitHub releases
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- For standard functions
+      "MunifTanjim/nui.nvim", -- To build the plugin UI
+      "nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+    },
+    config = true,
+  },
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+ __  __ _____ _____        __  __  __ _____ _____        __    _____
+|  \/  | ____/ _ \ \      / / |  \/  | ____/ _ \ \      / /  _|___ /
+| |\/| |  _|| | | \ \ /\ / /  | |\/| |  _|| | | \ \ /\ / /  (_) |_ \
+| |  | | |__| |_| |\ V  V /   | |  | | |__| |_| |\ V  V /    _ ___) |
+|_|  |_|_____\___/  \_/\_/    |_|  |_|_____\___/  \_/\_/    (_)____/
+
+]],
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
+      },
+    },
+  },
+}
