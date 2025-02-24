5948fc5936b2f93597bc543199e41fa610ee5366
aerithnetzer <aerithnetzer@outlook.com>
2025-02-24 11:22:20 -0600

    Push updates to init.lua

--- init.lua
+++ init.lua
@@ -4,47 +4,69 @@ require("config.lazy")
 -- boostrap mini plugins
 require("mini.comment").setup({})
 require("mini.icons").setup({})
-require("mini.surround").setup()
-require("mini.move").setup()
-
+require("mini.surround").setup({})
+require("mini.move").setup({
+  mappings = {
+    left = "H",
+    right = "L",
+    down = "J",
+    up = "K",
+  },
+})
 -- boostrap LSP and LSP server config
 require("lspconfig").ruff.setup({})
 require("lspconfig").pyright.setup({})
 require("lspconfig").tinymist.setup({})
+require("lspconfig").markdown_oxide.setup({})
 require("lualine").setup({
   options = {
     theme = {
       normal = {
         a = { fg = "#ffffff", bg = "#0000ff", gui = "bold" },
-        b = { fg = "#ffffff", bg = "#000080" },
-        c = { fg = "#ffffff", bg = "#000040" },
+        b = { fg = "#ffffff", bg = "#000000" },
+        c = { fg = "#ffffff", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
       insert = {
         a = { fg = "#000000", bg = "#00ff00", gui = "bold" },
-        b = { fg = "#000000", bg = "#008000" },
-        c = { fg = "#000000", bg = "#004000" },
+        b = { fg = "#000000", bg = "#000000" },
+        c = { fg = "#000000", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
       visual = {
         a = { fg = "#000000", bg = "#ff0000", gui = "bold" },
-        b = { fg = "#000000", bg = "#800000" },
-        c = { fg = "#000000", bg = "#400000" },
+        b = { fg = "#000000", bg = "#000000" },
+        c = { fg = "#000000", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
       replace = {
         a = { fg = "#000000", bg = "#ffff00", gui = "bold" },
-        b = { fg = "#000000", bg = "#808000" },
-        c = { fg = "#000000", bg = "#404000" },
+        b = { fg = "#000000", bg = "#000000" },
+        c = { fg = "#000000", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
       command = {
         a = { fg = "#000000", bg = "#ff00ff", gui = "bold" },
-        b = { fg = "#000000", bg = "#800080" },
-        c = { fg = "#000000", bg = "#400040" },
+        b = { fg = "#000000", bg = "#000000" },
+        c = { fg = "#000000", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
       inactive = {
         a = { fg = "#808080", bg = "#303030", gui = "bold" },
-        b = { fg = "#808080", bg = "#303030" },
-        c = { fg = "#808080", bg = "#303030" },
+        b = { fg = "#808080", bg = "#000000" },
+        c = { fg = "#808080", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
+      },
+      terminal = {
+        a = { fg = "#000000", bg = "#00ffff", gui = "bold" },
+        b = { fg = "#000000", bg = "#000000" },
+        c = { fg = "#000000", bg = "#000000" },
+        z = { fg = "#808080", bg = "#000000" },
       },
     },
   },
 })
 vim.api.nvim_set_keymap("t", "<Esc><Esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Change the background color to #000000
+vim.cmd("highlight Normal guibg=#000000")
