42f8df914e8af04ac12af6f5b068c270730e794e
Aerith Netzer <aerithnetzer@outlook.com>
2025-06-07 16:55:40 -0500

    Update nvim module

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,6 +1,25 @@
 return {
-	"m4xshen/hardtime.nvim",
-	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
-	opts = {},
+	{
+		"m4xshen/hardtime.nvim",
+		lazy = false,
+		dependencies = { "MunifTanjim/nui.nvim" },
+		opts = {},
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = function(_, opts)
+			local servers = { "pyright" }
+			for _, server in ipairs(servers) do
+				opts.servers[server] = opts.servers[server] or {}
+			end
+		end,
+	},
 }
