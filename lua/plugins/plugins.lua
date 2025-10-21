5e08ab399a6fea3ab17afdd13a9f3ba45523c13b
Aerith Netzer <aerithnetzer@outlook.com>
2025-10-21 14:45:26 -0500

    Update plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -49,4 +49,6 @@ return {
 			{ "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
 		},
 	},
+	{ "miikanissi/modus-themes.nvim", priority = 1000 },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
